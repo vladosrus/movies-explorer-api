a42c59d80a97bee22b48302bf53b3bbcf88f9127
Vlad Chikov <chikovvlad@yandex.com>
2022-10-20 15:16:51 +0300

    feat: создать необходимые роуты

--- app.js
+++ app.js
@@ -9,9 +9,25 @@ const app = express();
 const { PORT = 3000, MONGODB_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env; // пока что по умолчанию, потом скрыть
 const mongoose = require('mongoose');
 
+// Импортируем роутеры
+const routerSignUp = require('./routes/signUp');
+const routerSignIn = require('./routes/signIn');
+const routerUsers = require('./routes/users');
+const routerMovies = require('./routes/movies');
+const routerError = require('./routes/error');
+
 // Подключение к серверу mongoDB
 mongoose.connect(MONGODB_URL, {
   useNewUrlParser: true,
 });
 
-app.listen(PORT, () => console.log('запущено'));
+// Основные роуты
+app.use('/', routerSignUp);
+app.use('/', routerSignIn);
+app.use('/', routerUsers);
+app.use('/', routerMovies);
+
+// Обработка неправильного пути
+app.use('*', routerError);
+
+app.listen(PORT);
