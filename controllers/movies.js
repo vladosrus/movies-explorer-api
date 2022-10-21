5f12aa11ea8c99a157c6ca27db1500d0bed488a2
Vlad Chikov <chikovvlad@yandex.com>
2022-10-21 11:33:21 +0300

    feat: создать контроллер createMovie

---
+++ controllers/movies.js
@@ -0,0 +1,38 @@
+const Movie = require('../models/movie');
+
+const createMovie = (req, res) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
+
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    owner: req.user._id,
+    movieId,
+    nameRU,
+    nameEN,
+  })
+    .then((movie) => res.send(movie))
+    .catch((err) => res.send(err));
+};
+
+module.exports = {
+  createMovie,
+};
