18da8459d3457da009e5d652bf03968eca5400a2
Vlad Chikov <chikovvlad@yandex.com>
2022-10-22 15:09:49 +0300

    feat: реализовать защиту роутов авторизацией

---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/unauthorizedError');
+
+const { NODE_ENV, JWT_SECRET_KEY } = process.env;
+
+module.exports = (req, res, next) => {
+  const token = req.cookies.jwt;
+  let payload;
+
+  if (!token) {
+    next(new UnauthorizedError('Необходимо авторизоваться'));
+  }
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET_KEY : 'dev-secret');
+  } catch (err) {
+    next(new UnauthorizedError('Необходимо авторизоваться'));
+  }
+
+  req.user = payload;
+
+  next();
+};
