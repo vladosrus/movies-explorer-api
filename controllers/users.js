2942583ed8e162461a928ac205437ab7cfbc7b6f
Vlad Chikov <chikovvlad@yandex.com>
2022-10-22 12:10:46 +0300

    feat: создать контроллер login

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,13 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 const ConflictError = require('../errors/conflictError');
 const BadRequestError = require('../errors/badRequestError');
 const NotFoundError = require('../errors/notFoundError');
+
+const { NODE_ENV, JWT_SECRET_KEY } = process.env;
 
 const createUser = (req, res, next) => {
   const {
@@ -71,8 +75,31 @@ const updateUser = (req, res, next) => {
     });
 };
 
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET_KEY : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+      res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: true,
+        })
+        .send({ message: 'Авторизация прошла успешно', token })
+        .end();
+    })
+    .catch(next);
+};
+
 module.exports = {
   createUser,
   getUser,
   updateUser,
+  login,
 };
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const BadRequestError = require('../errors/badRequestError');
+const UnauthorizedError = require('../errors/unauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -27,5 +30,28 @@ const userSchema = new mongoose.Schema({
 }, {
   versionKey: false,
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  if (!email || !password) {
+    throw new BadRequestError('Переданы некорректные данные');
+  }
+
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Неправильные почта или пароль');
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Неправильные почта или пароль');
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
