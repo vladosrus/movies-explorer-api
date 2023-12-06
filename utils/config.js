665c76a1442f7c9cb97b24eec9e3bb63284ba80d
Vlad Chikov <chikovvlad@yandex.com>
2023-12-06 16:24:00 +0300

    feat: настроить работу с базой данных в зависимости от переменных окружения

--- utils/config.js
+++ utils/config.js
@@ -9,7 +9,7 @@ module.exports = {
   JWT_SECRET_KEY: NODE_ENV === 'production' ? JWT_SECRET_KEY : 'dev-secret',
   MONGODB_URL:
     NODE_ENV === 'production'
-      ? MONGODB_URL
-      : 'mongodb://localhost:27017/moviesdb',
-  PORT: NODE_ENV === 'production' ? PORT : 3000,
+      ? 'mongodb://localhost:27017/moviesdb'
+      : MONGODB_URL,
+  PORT: NODE_ENV === 'production' ? PORT : 3001,
 };
